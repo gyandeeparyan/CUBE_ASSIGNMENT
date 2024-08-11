f6bf5736357e9077ed39bf16b57249ab242520f2
Gyandeep Aryan <gyandeeparyan.dev@gmail.com>
2024-08-11 17:54:41 +0530

    first commit

---
+++ src/components/PhotoGrid.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PhotoGrid = () => {
+  return (
+    <div>PhotoGrid</div>
+  )
+}
+
+export default PhotoGrid
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const RANDOM_USER_URL=""
+export const UNSPLASH_IMG_URL=""
